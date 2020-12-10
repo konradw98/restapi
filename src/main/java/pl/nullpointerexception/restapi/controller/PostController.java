dced53af17d9fa128293452eb170535ac850df21
konrad <konradwodnicki98@gmail.com>
2020-12-10 14:51:51 +0100

    po odcinku 3

---
+++ src/main/java/pl/nullpointerexception/restapi/controller/PostController.java
@@ -0,0 +1,31 @@
+package pl.nullpointerexception.restapi.controller;
+
+import lombok.RequiredArgsConstructor;
+import pl.nullpointerexception.restapi.model.Post;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import pl.nullpointerexception.restapi.service.PostService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+
+
+    @GetMapping("/posts")
+    public List<Post> getPosts(){
+        return  postService.getPosts();
+
+    }
+
+    @GetMapping("/posts/{id}")
+    public Post getSinglePosts(@PathVariable long id){
+        return postService.getSinglePost(id);
+
+    }
+}
